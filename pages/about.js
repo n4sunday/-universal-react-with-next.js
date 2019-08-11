aec0649d62a29078de88289e2b01b549df3c02c6
Sunday <nsoooooooooosn@gmail.com>
2019-08-12 00:11:30 +0700

    Custom Error Page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import fetch from 'isomorphic-unfetch'
+import Error from './_error'
 import Layout from '../components/Layout'
 
 class About extends Component {
     static async getInitialProps() {
         const res = await fetch('https://api.github.com/users/n4sunday')
+        const statusCode = res.status > 200 ? res.status : false
         const data = await res.json()
 
-        return { user: data}
+        return { user: data, statusCode}
     }
 
     render() {
-        const { user } = this.props
+        const { user, statusCode } = this.props
+        if(statusCode) {
+            return <Error statusCode={statusCode} />
+        }
+
         return (
             <Layout title="About">
                 About
