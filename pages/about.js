c6b78746acf2fc3e23ae1589baa3eee7a425e6ea
Sunday <nsoooooooooosn@gmail.com>
2019-08-12 00:03:51 +0700

    Fetching Data with getInitailProps

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,25 @@
+import React, { Component } from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '../components/Layout'
-const About = () => {
-    return (
-        <Layout title="About">
-            <div>About</div>
-        </Layout>
-        
-    )
+
+class About extends Component {
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/n4sunday')
+        const data = await res.json()
+
+        return { user: data}
+    }
+
+    render() {
+        const { user } = this.props
+        return (
+            <Layout title="About">
+                About
+                <p>{user.name}</p>
+                <img src={user.avatar_url} alt="Me" height="200px" />
+            </Layout>
+        )
+    }
 }
 
-export default About
+export default About;
